3bf850b74578b21b3065bad18ad0936c4cc82b90
Dániel Borzai <daniel.borzai@stud.uni-corvinus.hu>
2020-10-11 15:43:39 +0200

    week05_part04

--- feladat_week05/feladat_week05/Form1.cs
+++ feladat_week05/feladat_week05/Form1.cs
@@ -12,9 +12,14 @@ namespace feladat_week05
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
