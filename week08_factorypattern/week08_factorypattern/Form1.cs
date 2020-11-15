cd3f63a2a21d07ab566b1100eefa520d597959bb
Dániel Borzai <daniel.borzai@stud.uni-corvinus.hu>
2020-11-15 16:21:11 +0100

    week08_part11

--- week08_factorypattern/week08_factorypattern/Form1.cs
+++ week08_factorypattern/week08_factorypattern/Form1.cs
@@ -16,12 +16,17 @@ namespace week08_factorypattern
     {
         private List<Toy> _toys = new List<Toy>();
 
-        private IToyFactory _factory;
+        private Toy _nextToy;
 
+        private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -54,6 +59,31 @@ namespace week08_factorypattern
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
