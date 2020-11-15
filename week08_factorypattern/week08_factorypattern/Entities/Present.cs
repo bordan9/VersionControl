61cb9d3ce19e2b63eed275f8a22b6b461ab577a4
Dániel Borzai <daniel.borzai@stud.uni-corvinus.hu>
2020-11-15 18:24:47 +0100

    week08_part14

---
+++ week08_factorypattern/week08_factorypattern/Entities/Present.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week08_factorypattern.Abstractions;
+using System.Drawing;
+
+namespace week08_factorypattern.Entities
+{
+    public class Present : Toy
+    {
+        public SolidBrush RibbonColor { get; private set; }
+        public SolidBrush BoxColor { get; private set; }
+
+        protected override void DrawImage(Graphics g)
+        {
+            
+        }
+    }
+}
