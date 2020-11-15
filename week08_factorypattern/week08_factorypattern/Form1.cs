0b268161781aef34371d1e3f06af57de8b4308f2
Dániel Borzai <daniel.borzai@stud.uni-corvinus.hu>
2020-11-15 17:39:43 +0100

    week08_part13

--- week08_factorypattern/week08_factorypattern/Form1.cs
+++ week08_factorypattern/week08_factorypattern/Form1.cs
@@ -73,7 +73,10 @@ namespace week08_factorypattern
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
         }
 
         private void DisplayNext()
