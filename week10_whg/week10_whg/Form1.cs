eae02eba8e10615a0281618e4b3510b1e737bb7a
Dániel Borzai <daniel.borzai@stud.uni-corvinus.hu>
2020-11-28 15:49:14 +0100

    week10_part07

--- week10_whg/week10_whg/Form1.cs
+++ week10_whg/week10_whg/Form1.cs
@@ -20,6 +20,8 @@ namespace week10_whg
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -51,6 +53,16 @@ namespace week10_whg
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
 
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
